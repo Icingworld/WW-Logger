eba04bfd64532003601d3a5193db4cc064682c1e
icingworld <984442022@qq.com>
2025-04-23 17:32:55 +0800

    feat: create async worker

---
+++ logger/include/Logger/AsyncWorker.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+#include <Logger/LoggerBuffer.h>
+
+namespace WW
+{
+
+/**
+ * @brief 异步日志管理线程
+ */
+class AsyncWorker
+{
+private:
+    
+};
+
+} // namespace WW
---
+++ logger/src/Logger/AsyncWorker.cpp
@@ -0,0 +1,8 @@
+#include "AsyncWorker.h"
+
+namespace WW
+{
+
+
+
+} // namespace WW
