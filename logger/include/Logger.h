e57c65ffe740d9598e32cd2e05410643382d4c23
icingworld <984442022@qq.com>
2025-04-23 10:29:06 +0800

    feat: improve Logger

--- logger/include/Logger.h
+++ logger/include/Logger.h
@@ -1,8 +1,9 @@
 #pragma once
 
-#include "SyncLogger.h"
-#include "AsyncLogger.h"
-#include "Formatter.h"
+#include <unordered_map>
+
+#include <Logger/SyncLogger.h>
+#include <Logger/AsyncLogger.h>
 
 namespace WW
 {
@@ -15,15 +16,18 @@ class Logger
 private:
     LogLevel level;                         // 日志等级
     std::shared_ptr<LoggerBase> logger;     // 日志实例
+    std::string name;                       // 日志名称
+    static std::mutex loggers_mutex;        // 日志哈希表互斥锁
+    static std::unordered_map<std::string, std::shared_ptr<Logger>> loggers;    // 日志哈希表
 
 private:
-    Logger();
+    explicit Logger(const std::string & name);
 
-    explicit Logger(LogLevel level);
+    Logger(const std::string & name, LogLevel level);
 
-    explicit Logger(LogType type);
+    Logger(const std::string & name, LogType type);
 
-    Logger(LogLevel level, LogType type);
+    Logger(const std::string & name, LogLevel level, LogType type);
 
     Logger(const Logger &) = delete;
 
@@ -37,9 +41,19 @@ public:
     ~Logger();
 
     /**
-     * @brief 获取日志实例
+     * @brief 获取默认日志实例
      */
-    static Logger & getLogger();
+    static Logger & getDefaultLogger(const std::string & name = "default_logger");
+
+    /**
+     * @brief 获取同步日志实例
+     */
+    static Logger & getSyncLogger(const std::string & name = "sync_logger");
+
+    /**
+     * @brief 获取异步日志实例
+     */
+    static Logger & getAsyncLogger(const std::string & name = "async_logger");
 
     /**
      * @brief 设置日志类型
@@ -54,7 +68,7 @@ public:
     /**
      * @brief 添加日志输出
      */
-    void addSink(std::shared_ptr<Sink> sink);
+    void addSink(std::shared_ptr<SinkBase> sink);
 
     /**
      * @brief 输出日志
@@ -62,9 +76,45 @@ public:
     void log(LogLevel level, const std::string & message, const char * file = "", unsigned int line = 0, const char * function = "");
 
     /**
+     * @brief 输出跟踪日志
+     */
+    void trace(const std::string & message, const char * file = "", unsigned int line = 0, const char * function = "");
+
+    /**
+     * @brief 输出调试日志
+     */
+    void debug(const std::string & message, const char * file = "", unsigned int line = 0, const char * function = "");
+
+    /**
+     * @brief 输出信息日志
+     */
+    void info(const std::string & message, const char * file = "", unsigned int line = 0, const char * function = "");
+
+    /**
+     * @brief 输出警告日志
+     */
+    void warn(const std::string & message, const char * file = "", unsigned int line = 0, const char * function = "");
+
+    /**
+     * @brief 输出错误日志
+     */
+    void error(const std::string & message, const char * file = "", unsigned int line = 0, const char * function = "");
+
+    /**
+     * @brief 输出致命日志
+     */
+    void fatal(const std::string & message, const char * file = "", unsigned int line = 0, const char * function = "");
+
+    /**
      * @brief 刷新日志输出
      */
     void flush();
+
+private:
+    /**
+     * @brief 获取日志实例
+     */
+    static Logger & getLogger(const std::string & name, LogType type);
 };
 
 } // namespace WW
--- logger/src/Logger.cpp
+++ logger/src/Logger.cpp
@@ -3,23 +3,28 @@
 namespace WW
 {
 
-Logger::Logger()
-    : Logger(LogLevel::Info, LogType::Sync)
+std::mutex Logger::loggers_mutex;
+
+std::unordered_map<std::string, std::shared_ptr<Logger>> loggers;
+
+Logger::Logger(const std::string & name)
+    : Logger(name, LogLevel::Info, LogType::Sync)
 {
 }
 
-Logger::Logger(LogLevel level)
-    : Logger(level, LogType::Sync)
+Logger::Logger(const std::string & name, LogLevel level)
+    : Logger(name, level, LogType::Sync)
 {
 }
 
-Logger::Logger(LogType type)
-    : Logger(LogLevel::Info, type)
+Logger::Logger(const std::string & name, LogType type)
+    : Logger(name, LogLevel::Info, type)
 {
 }
 
-Logger::Logger(LogLevel level, LogType type)
+Logger::Logger(const std::string & name, LogLevel level, LogType type)
     : level(level)
+    , name(name)
 {
     if (type == LogType::Sync) {
         logger = std::make_shared<SyncLogger>();
@@ -33,10 +38,19 @@ Logger::~Logger()
     flush();
 }
 
-Logger & Logger::getLogger()
+Logger & Logger::getDefaultLogger(const std::string & name)
 {
-    static Logger instance;
-    return instance;
+    return getLogger(name, LogType::Sync);
+}
+
+Logger & Logger::getSyncLogger(const std::string & name)
+{
+    return getLogger(name, LogType::Sync);
+}
+
+Logger & Logger::getAsyncLogger(const std::string & name)
+{
+    return getLogger(name, LogType::Async);
 }
 
 void Logger::setType(LogType type)
@@ -58,7 +72,7 @@ void Logger::setLevel(LogLevel level)
     this->level = level;
 }
 
-void Logger::addSink(std::shared_ptr<Sink> sink)
+void Logger::addSink(std::shared_ptr<SinkBase> sink)
 {
     logger->addSink(sink);
 }
@@ -71,9 +85,52 @@ void Logger::log(LogLevel level, const std::string & message, const char * file,
     logger->log(level, message, file, line, function);
 }
 
+void Logger::trace(const std::string & message, const char * file, unsigned int line, const char * function)
+{
+    log(LogLevel::Trace, message, file, line, function);
+}
+
+void Logger::debug(const std::string & message, const char * file, unsigned int line, const char * function)
+{
+    log(LogLevel::Debug, message, file, line, function);
+}
+
+void Logger::info(const std::string & message, const char * file, unsigned int line, const char * function)
+{
+    log(LogLevel::Info, message, file, line, function);
+}
+
+void Logger::warn(const std::string & message, const char * file, unsigned int line, const char * function)
+{
+    log(LogLevel::Warn, message, file, line, function);
+}
+
+void Logger::error(const std::string & message, const char * file, unsigned int line, const char * function)
+{
+    log(LogLevel::Error, message, file, line, function);
+}
+
+void Logger::fatal(const std::string & message, const char * file, unsigned int line, const char * function)
+{
+    log(LogLevel::Fatal, message, file, line, function);
+}
+
 void Logger::flush()
 {
     logger->flush();
+}
+
+Logger & Logger::getLogger(const std::string & name, LogType type)
+{
+    std::lock_guard<std::mutex> lock(loggers_mutex);
+    auto it = loggers.find(name);
+    if (it == loggers.end()) {
+        auto logger = std::make_shared<Logger>(name, type);
+        loggers[name] = logger;
+        return *logger;
+    }
+
+    return *(it->second);
 }
 
 } // namespace WW
